456f7497c4030b2772db729ad1c293d0e9516f90
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-15 16:53:11 -0230

    Add models

--- school/database/models.py
+++ school/database/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.subject}"
+
+class Grade(models.Model):
+    name = models.CharField(max_length=5)
+    head_teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    subjects = models.ManyToManyField("Subject")
+
+    def __str__(self):
+        return f"{self.name}, {self.head_teacher}"
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    grade = models.ForeignKey(Grade, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.name}, {Grade.objects.get(id=self.grade).name}"
