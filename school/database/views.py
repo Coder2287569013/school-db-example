472346c2c938a83a78560a80e0d13d797d3acd1e
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-18 09:18:16 -0230

    Commit

--- school/database/views.py
+++ school/database/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from database import models
 
 # Create your views here.
+class StudentsListView(ListView):
+    model = models.Student
+    context_object_name = "students"
+    template_name = "students-list.html"
