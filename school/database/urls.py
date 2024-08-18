848cd1e8b85d89b396fbf08b438d94ae6b5a3866
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-18 09:58:40 -0230

    Another commit

--- school/database/urls.py
+++ school/database/urls.py
@@ -3,5 +3,6 @@ from database import views
 
 urlpatterns = [
     path('students-list/', views.StudentsListView.as_view(), name='students-list'),
-    path('teachers-list/', views.teacher_list, name='teachers-list')
+    path('teachers-list/', views.teacher_list, name='teachers-list'),
+    path('student-detail/<int:pk>/', views.student_details, name='student-detail')
 ]
--- school/database/views.py
+++ school/database/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.http import HttpResponse
 from database import models
 
 # Create your views here.
@@ -16,3 +17,16 @@ def teacher_list(request):
     }
     
     return render(request, "database/teachers-list.html", context)
+
+
+def student_details(request, pk):
+    try:
+        student = models.Student.objects.get(id=pk)
+    except models.Student.DoesNotExist:
+        HttpResponse("Student doesn't exist", status=404)
+ 
+    context = {
+        "student_info": student
+    }
+
+    return render(request, "database/student-details.html", context)
